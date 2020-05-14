d746e9164a8d8f0536be42f6b80b165d8157d3c0
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-05-14 20:02:47 +0100

    Start the react complete guide course on Udemy. Make a simple react app with two approaches; class based components and function based components.

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+// Importing the useState hook for 'function' based components
+// import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+    // The state property is only available within class-based components that extend the React Component class.
+    // Be careful using state though, it can quickly become unpredictable and difficult to manage if it's used
+    // throughout lots of components in your app.
+    state = {
+        persons: [
+            { name: 'Charlie', age: 25 },
+            { name: 'Max', age: 28 },
+            { name: 'Amy', age: 24 }
+        ],
+        otherState: 'Some other value'
+    };
+
+    // Handler convention is useful here to denote that this method is not being actively called, but rather, on an event.
+    switchNameHandler = () => {
+        // DONT DO THIS -> this.state.persons[0].name = "Charles";
+
+        // setState is a special method that allows the state object to be updated, and where possible, merges data.
+        this.setState({
+            persons: [
+                { name: 'Charles', age: 25 },
+                { name: 'Max', age: 28 },
+                { name: 'Amy', age: 27 }
+            ]
+        })
+    };
+
+    render() {
+        return (
+            // Here, we shouldn't try to render adjacent elements. JSX is built to render elements with a single container.
+            <div className="App">
+                <h1>Hi, I'm a React app.</h1>
+
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+
+                {/* Importing the 'Person' component using custom 'props' on each i.e. 'name' and 'age' */}
+                {/* 'Props' are how we pass data down into the component. Changes in these props can trigger a UI update */}
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Riding my motorbike</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        );
+
+        // This is essentially what the render() call above does using JSX.
+        // return(React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React app.')));
+    }
+}
+
+/* FUNCTION BASED ALTERNATIVE - The below is the 'hook' based approach to updating state via functional based components.
+ It's the most current way of handling state, but is not the most adopted. */
+
+// const app = props => {
+//     // The useState() 'hook' function allows us to add state to our components. It always returns exactly two elements.
+//     // The first is the state object, the second is a function that allows you to update the state.
+//
+//     // One of the big caveats of using functional based components, is that unlike class based components, state is replaced
+//     // when it is updated. Therefore, it is your job to ensure you include all old state data.
+//     const [ personsState, setPersonsState ] = useState({
+//         persons: [
+//             { name: 'Charlie', age: 25 },
+//             { name: 'Max', age: 28 },
+//             { name: 'Amy', age: 24 }
+//         ]
+//         // Below would get overwritten if only the persons array was updated on setPersonsState
+//         // otherState: 'Some other value'
+//     });
+//
+//     // In order to prevent state data getting replaced accidentally, it is perhaps better to use multiple useState()
+//     // function calls, with destructuring, to ensure the data isn't lost.
+//     const [othersState, setOthersState] = useState('Some other value');
+//
+//     console.log(personsState, othersState);
+//
+//     // Handler convention is useful here to denote that this method is not being actively called, but rather, on an event.
+//     const switchNameHandler = () => {
+//         // DONT DO THIS -> personsState.persons[0].name = "Charles";
+//
+//         setPersonsState({
+//             persons: [
+//                 { name: 'Charles', age: 25 },
+//                 { name: 'Max', age: 28 },
+//                 { name: 'Amy', age: 27 }
+//             ]
+//         });
+//
+//         setOthersState('Now I\'ve changed state');
+//     }
+//
+//     return (
+//         // Here, we shouldn't try to render adjacent elements. JSX is built to render elements with a single container.
+//         <div className="App">
+//             <h1>Hi, I'm a React app.</h1>
+//
+//             <button onClick={switchNameHandler}>Switch Name</button>
+//
+//             {/* Importing the 'Person' component using custom 'props' on each i.e. 'name' and 'age' */}
+//             {/* 'Props' are how we pass data down into the component. Changes in these props can trigger a UI update */}
+//             <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//             <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies: Riding my motorbike</Person>
+//             <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+//         </div>
+//     );
+//
+//     // This is essentially what the render() call above does using JSX.
+//     // return(React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React app.')));
+// }
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/* This is an example of a 'stateless' component as it only outputs external data. It's good to use as many stateless
+components as possible. Keeping a minimal amount of 'stateful' components makes the app much easier to maintain and
+keeps a clear flow of data from the 'stateful' container component, down to the 'stateless' presentational component. */
+
+// Props are passed into the call for the component i.e. <person name="Charlie" age="25" />
+const person = (props) => {
+    //  Using the curly braces, we can evaluate single line expressions.
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old!.</p>
+
+            {/* Children is a reserved word. It outputs any elements passed in between the opening and closing tags
+            of the component usage */}
+            <p>{props.children}</p>
+        </div>
+    )
+};
+
+export default person;
