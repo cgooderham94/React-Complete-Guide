1e173f44b9edf21bcd6d88837fb928bc7ecf6f34
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-05-25 20:35:32 +0100

    Add function to toggle display of persons components, using conditional rendering.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
             { name: 'Max', age: 28 },
             { name: 'Amy', age: 24 }
         ],
-        otherState: 'Some other value'
+        otherState: 'Some other value',
+        showPersons: false
     };
 
     // Handler convention is useful here to denote that this method is not being actively called, but rather, on an event.
@@ -42,6 +43,12 @@ class App extends Component {
         })
     };
 
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+        console.log("hello!")
+    };
+
     render() {
         const style = {
             padding: '0.5rem',
@@ -60,31 +67,36 @@ class App extends Component {
                 {/* This is binding the context of the class 'App' to the function. It also passes 'Charles' as an
                 argument to the switchNameHandler function */}
                 <button style={style}
-                        onClick={this.switchNameHandler.bind(this, 'Charles')}>Switch Name</button>
+                        onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-                {/* Importing the 'Person' component using custom 'props' on each i.e. 'name' and 'age' */}
-                {/* 'Props' are how we pass data down into the component. Changes in these props can trigger a UI update */}
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age} />
+                {/* React doesn't have directives like Vue (v-if). Instead, we use a simple because evaluation to render */}
+                { this.state.showPersons === true ?
+                    <div>
+                        {/* Importing the 'Person' component using custom 'props' on each i.e. 'name' and 'age' */}
+                        {/* 'Props' are how we pass data down into the component. Changes in these props can trigger a UI update */}
+                        <Person
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age} />
 
-                {/* It is possible to pass a method down into a component by setting it as a prop. This is a common pattern
-                 in react apps and allows the component to still alter state, without being 'stateful' itself. */}
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
+                        {/* It is possible to pass a method down into a component by setting it as a prop. This is a common pattern
+                        in react apps and allows the component to still alter state, without being 'stateful' itself. */}
+                        <Person
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}
 
-                    /* This is an alternative to binding the context to the switchNameHandler function. Be aware,
-                    though, it's not as performant so use it wisely (React can sometimes re-render things too often.
-                    While we wouldn't ordinarily 'call' the function in the prop, when using an arrow function, it
-                    implicitly adds a 'return' before the function body. Hence, the function wont actually get called
-                    until a click is registered. */
-                    click={() => this.switchNameHandler('Maximillian!!')}
-                    changed={this.nameChangedHandler}>My hobbies: Riding my motorbike</Person>
+                            /* This is an alternative to binding the context to the switchNameHandler function. Be aware,
+                            though, it's not as performant so use it wisely (React can sometimes re-render things too often.
+                            While we wouldn't ordinarily 'call' the function in the prop, when using an arrow function, it
+                            implicitly adds a 'return' before the function body. Hence, the function wont actually get called
+                            until a click is registered. */
+                            click={() => this.switchNameHandler('Maximillian!!')}
+                            changed={this.nameChangedHandler}>My hobbies: Riding my motorbike</Person>
 
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age} />
+                        <Person
+                            name={this.state.persons[2].name}
+                            age={this.state.persons[2].age} />
+                    </div> : null
+                }
             </div>
         );
 
