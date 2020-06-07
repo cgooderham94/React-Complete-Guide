f9c587d8db478393628b6f9acebdda835906d620
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-06-07 22:15:35 +0100

    Restructure app.js to minimise UI content within the tempalte. Abstract markup to components.

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+    const assignedClasses = [];
+    let btnClass = '';
+
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if(props.persons.length <= 2) {
+        assignedClasses.push('red');
+    }
+
+    if (props.persons.length <= 1) {
+        assignedClasses.push('bold');
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1>Hi, I'm a React app.</h1>
+
+            <p className={assignedClasses.join(" ")}>It's amazing right? I'm working!</p>
+
+            {/* This is binding the context of the class 'App' to the function. It also passes 'Charles' as an
+                    argument to the switchNameHandler function */}
+            <button className={btnClass} onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+}
+
+export default cockpit;
---
+++ src/containers/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+
+import Cockpit from '../components/Cockpit/Cockpit'
+import Persons from '../components/Persons/Persons';
+
+class App extends Component {
+    // The state property is only available within class-based components that extend the React Component class.
+    // Be careful using state though, it can quickly become unpredictable and difficult to manage if it's used
+    // throughout lots of components in your app.
+    state = {
+        persons: [
+            { id: 'asdikh', name: 'Charlie', age: 25 },
+            { id: 'asf', name: 'Max', age: 28 },
+            { id: 'asgtqw', name: 'Amy', age: 24 }
+        ],
+        otherState: 'Some other value',
+        showPersons: false
+    };
+
+    // This handler is invoked on text input 'onChange' event. It grabs the value of the text input.
+    nameChangedHandler = (event, id) => {
+        /* Find the person who's data is being changed, using their array index */
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        // 'Spread' the person object who's data we've changed into a new object
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        // Set the inputs value as the persons name
+        person.name = event.target.value;
+
+        // Spread the existing persons array into a new one and add the updated persons data
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        // Set the state to the new persons array
+        this.setState({
+            persons: persons
+        })
+    };
+
+    deletePersonHandler = (personIndex) => {
+        // This sets a reference to React's original array of persons. It is highly recommended not to manipulate
+        // original data, but rather a copy of the data (shown under the bad example). Splicing without arguments simply
+        // creates a duplicate array.
+        // const persons = this.state.persons;
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
+    };
+
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    };
+
+    render() {
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (<Persons
+                    persons={this.state.persons}
+                    clicked={this.deletePersonHandler}
+                    changed={this.nameChangedHandler}/>
+            );
+        }
+
+        return (
+            // Here, we shouldn't try to render adjacent elements. JSX is built to render elements with a single container.
+            <div className={classes.App}>
+                <Cockpit showPersons={this.state.showPersons}
+                         persons={this.state.persons}
+                         clicked={this.togglePersonsHandler}/>
+
+                { /* This is a much cleaner and tidier way of outputting conditional content. It keeps the core return of
+                this class clean and readable. */ }
+                {persons}
+            </div>
+        );
+
+        // This is essentially what the render() call above does using JSX.
+        // return(React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React app.')));
+    }
+}
+
+export default App;
