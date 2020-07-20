18d2f94c015bfec80e2c2cfa0dc013b6e3398f0c
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-07-20 16:41:15 +0100

    Create a new higher order component; Aux. Use this to wrap JSX elements where you don't require a single top level element.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Aux';
+
 // import './Person.css';
 import styled from 'styled-components'
 
@@ -26,18 +29,20 @@ class Person extends Component {
         console.log('[Person.js] rendering...')
 
         //  Using the curly braces, we can evaluate single line expressions.
-        return [
+        return (
+            <Aux>
                 <p key="I1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!.</p>,
 
-                /* Children is a reserved word. It outputs any elements passed in between the opening and closing tags
-            of the component usage */
+                {/* Children is a reserved word. It outputs any elements passed in between the opening and closing tags */}
+                {/* of the component usage */}
                 <p key="I2" onClick={this.props.click}>{this.props.children}</p>,
 
-                /* Without the value attribute here, you'd force the component into being 'read-only'. However, with the
-            onChange event added, you now have a two-way binding. This both allows the initial value to be a prop, but
-            also allow it to be dynamically updated too. */
+                {/* Without the value attribute here, you'd force the component into being 'read-only'. However, with the */}
+                {/* onChange event added, you now have a two-way binding. This both allows the initial value to be a prop, but */}
+                {/* also allow it to be dynamically updated too. */}
                 <input key="I3" type="text" onChange={this.props.changed} value={this.props.name} />
-        ];
+            </Aux>
+        );
     }
 }
 
