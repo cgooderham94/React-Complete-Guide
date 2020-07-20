f16292009d60b9de37c490a124c19949580e85a4
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-07-20 11:17:15 +0100

    Add an alert in a setTimeout within useEffect to exaggerate how often useEffect gets called.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+
+    // useEffect combines componentDidMount and componentDidUpdate together in one React Hook.
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // HTTP request...
+        setTimeout(() => {
+            alert('Saved data to cloud!');
+        }, 1000)
+    })
+
     const assignedClasses = [];
     let btnClass = '';
 
