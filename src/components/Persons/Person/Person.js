6eb5532da98c1db5e7f47b44f62bdf120ffbc622
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-07-25 15:27:08 +0100

    Implement withClass function in Person.js and modify withClass so that it can apply unknown props to the wrapped compomnent

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,24 +1,8 @@
 import React, { Component, Fragment } from 'react';
 
 import Aux from '../../../hoc/Aux';
-
-// import './Person.css';
-import styled from 'styled-components'
-
-const StyledDiv = styled.div`
-    width: 60%;
-    padding: 1rem;
-    margin: 1rem auto;
-    border: 2px solid #CCC;
-    background-color: #EEE;
-    box-shadow: 2px 4px 4px #DDD;
-    text-align: center;
-    transition: 0.2s ease-in-out;
-    
-    @media (min-width: 576px) {
-        width: 450px;
-    }
-`;
+import classes from './Person.css'
+import withClass from '../../../hoc/withClass'
 
 /* This is an example of a 'stateless' component as it only outputs external data. It's good to use as many stateless
 components as possible. Keeping a minimal amount of 'stateful' components makes the app much easier to maintain and
@@ -48,4 +32,4 @@ class Person extends Component {
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
