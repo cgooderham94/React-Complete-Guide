0f617bdfd6361d26821c5b024fe224bc7423f0dd
Charlie Gooderham <c.gooderham@euphoriq.co.uk>
2020-08-06 20:52:03 +0100

    Implement use of refs in a functional component (cockpit)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = props => {
+    // This is how you can set references in functional components.
+    const toggleButtonRef = useRef(null);
+
     // useEffect combines componentDidMount and componentDidUpdate together in one React Hook.
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
-        
+
+        toggleButtonRef.current.click();
+
         /* This cleanup work gets executed when the component is destroyed (or when data changes if data is added to
         the array), as a result of the empty array as the second argument in the useEffect call */
         return () => {
@@ -49,7 +54,7 @@ const cockpit = props => {
 
             {/* This is binding the context of the class 'App' to the function. It also passes 'Charles' as an
                     argument to the switchNameHandler function */}
-            <button className={btnClass} onClick={props.clicked}>Toggle Persons</button>
+            <button ref={toggleButtonRef} className={btnClass} onClick={props.clicked}>Toggle Persons</button>
         </div>
     );
 }
